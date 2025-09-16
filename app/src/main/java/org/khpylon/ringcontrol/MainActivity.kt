dfaa6d9937f832e02de086600f863a6b6b7618f2
Ken <ken.l.hughes@gmail.com>
2025-09-16 08:43:08 -0700

    Initial design of the app interface using Jetpack Compose.

--- app/src/main/java/org/khpylon/ringcontrol/MainActivity.kt
+++ app/src/main/java/org/khpylon/ringcontrol/MainActivity.kt
@@ -1,99 +1,133 @@
 package org.khpylon.ringcontrol
 
+import android.app.Activity.NOTIFICATION_SERVICE
 import android.app.NotificationManager
-import android.appwidget.AppWidgetManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.media.AudioManager
 import android.os.Bundle
 import android.provider.Settings
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import org.khpylon.ringcontrol.ui.theme.RingControlTheme
 
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        applicationContext?.registerReceiver(object : BroadcastReceiver() {
+        val context = applicationContext
+        context.registerReceiver(object : BroadcastReceiver() {
+
+            // Register a receiver to detect external changes to the ringer
             override fun onReceive(context: Context, intent: Intent) {
-                if (intent.action == "android.media.RINGER_MODE_CHANGED") {
+                if (intent.action == AudioManager.RINGER_MODE_CHANGED_ACTION) {
                     Widget.updateWidget(context)
                 }
             }
-        }, IntentFilter("android.media.RINGER_MODE_CHANGED"))
+        }, IntentFilter(AudioManager.RINGER_MODE_CHANGED_ACTION))
 
         enableEdgeToEdge()
         setContent {
             RingControlTheme {
-//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-//                    Greeting(
-//                        name = "Android",
-//                        modifier = Modifier.padding(innerPadding)
-//                    )
-//                }
-
-                val notificationManager =
-                    getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-
-                // the below code is to check the permission that the access notification policy settings from users device..
-                if (!notificationManager.isNotificationPolicyAccessGranted
-                ) {
-                    val intent = Intent(
-                        Settings
-                            .ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = { Text(context.getString(R.string.app_name)) }
+                        )
+                    }, modifier = Modifier.fillMaxSize()
+                )
+                { innerPadding ->
+                    MainApplication (
+                        modifier = Modifier.padding(innerPadding)
                     )
-                    startActivity(intent)
                 }
 
-//                val audioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
-//                val currentMode = audioManager.getRingerMode()
-//
-//                when (currentMode) {
-//                    AudioManager.RINGER_MODE_NORMAL -> audioManager.setRingerMode(AudioManager.RINGER_MODE_VIBRATE)
-//                    AudioManager.RINGER_MODE_VIBRATE -> audioManager.setRingerMode(AudioManager.RINGER_MODE_SILENT)
-//                    AudioManager.RINGER_MODE_SILENT -> audioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL)
-//                }
-
                 Widget.updateWidget(applicationContext)
-
-//                val receiver: BroadcastReceiver = object : BroadcastReceiver() {
-//                    override fun onReceive(context: Context, intent: Intent) {
-//                        val newIntent = Intent(context, Widget::class.java)
-//                        newIntent.action = "REFRESH"
-//                        context.sendBroadcast(newIntent)
-//                    }
-//                }
-//                val filter = IntentFilter(
-//                    AudioManager.RINGER_MODE_CHANGED_ACTION
-//                )
-//                registerReceiver(receiver, filter)
-
-                finish()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun MainApplication(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+
+    val notificationManager = context.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+    var permissions by remember { mutableStateOf(notificationManager.isNotificationPolicyAccessGranted) }
+    val launcher =
+        rememberLauncherForActivityResult(ActivityResultContracts.StartActivityForResult())
+        {
+            permissions = notificationManager.isNotificationPolicyAccessGranted
+        }
+
+    Column(
         modifier = modifier
-    )
+            .padding(horizontal = 10.dp) // add some space on left and right
+    ) {
+
+        // Description of the app
+        Text(
+            text = stringResource(R.string.app_explanation),
+            modifier = Modifier
+                .padding(10.dp)
+        )
+
+        // Toggle control for DND permissions
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+        )
+        {
+            Text(
+                text = stringResource(R.string.dnd_permissions),
+                modifier = Modifier
+                    .padding(10.dp)
+            )
+            Spacer(Modifier.weight(1f))  // separate text and toggle switch
+            Switch(
+                checked = permissions,
+                onCheckedChange = {
+                    val intent = Intent(Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS)
+                    launcher.launch(intent)
+                }
+            )
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     RingControlTheme {
-        Greeting("Android")
+        MainApplication()
     }
 }
