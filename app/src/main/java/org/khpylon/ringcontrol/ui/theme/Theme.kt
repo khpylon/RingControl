67c237b280a492fa358e2a540242b1807bb61ecf
Ken <ken.l.hughes@gmail.com>
2025-09-17 14:26:42 -0700

    Change theme.

--- app/src/main/java/org/khpylon/ringcontrol/ui/theme/Theme.kt
+++ app/src/main/java/org/khpylon/ringcontrol/ui/theme/Theme.kt
@@ -11,16 +11,42 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
+//private val DarkColorScheme = darkColorScheme(
+//    primary = Purple80,
+//    secondary = PurpleGrey80,
+//    tertiary = Pink80
+//)
+//
+//private val LightColorScheme = lightColorScheme(
+//    primary = Purple40,
+//    secondary = PurpleGrey40,
+//    tertiary = Pink40
+//
+//    /* Other default colors to override
+//    background = Color(0xFFFFFBFE),
+//    surface = Color(0xFFFFFBFE),
+//    onPrimary = Color.White,
+//    onSecondary = Color.White,
+//    onTertiary = Color.White,
+//    onBackground = Color(0xFF1C1B1F),
+//    onSurface = Color(0xFF1C1B1F),
+//    */
+//)
+
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    background = Black,
+    primary = Light_blue_900,
+    secondary = White,
+    tertiary = Light_blue_200,
+    onPrimary = Black
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    background = White,
+    primary = Light_blue_900,
+    secondary = Black,
+    tertiary = Light_blue_200,
+    onPrimary = Black
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
