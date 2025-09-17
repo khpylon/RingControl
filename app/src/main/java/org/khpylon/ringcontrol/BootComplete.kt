96793c5e4039306b996bb3eb8c4ee839cc0d70fc
Ken <ken.l.hughes@gmail.com>
2025-09-17 14:25:19 -0700

    Update widget on boot.

---
+++ app/src/main/java/org/khpylon/ringcontrol/BootComplete.kt
@@ -0,0 +1,16 @@
+package org.khpylon.ringcontrol
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BootComplete : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        val action = intent.action
+        if (action.equals(Intent.ACTION_BOOT_COMPLETED, ignoreCase = true) ||
+            action.equals(Intent.ACTION_MY_PACKAGE_REPLACED, ignoreCase = true) )
+        {
+            Widget.updateWidget(context)
+        }
+    }
+}
