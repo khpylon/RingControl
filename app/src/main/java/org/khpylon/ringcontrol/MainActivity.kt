a958725c2844df1d44e7197b2c4aff7ca116433a
Ken <ken.l.hughes@gmail.com>
2025-09-23 20:01:53 -0700

    Add support for displaying "changelog" info to user on new update.

--- app/src/main/java/org/khpylon/ringcontrol/MainActivity.kt
+++ app/src/main/java/org/khpylon/ringcontrol/MainActivity.kt
@@ -10,6 +10,7 @@ import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.content.res.AssetManager
 import android.graphics.drawable.Drawable
 import android.icu.text.MessageFormat
 import android.net.Uri
@@ -36,16 +37,25 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Slider
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -63,6 +73,8 @@ import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
@@ -89,7 +101,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         val context = applicationContext
-
 
         // Older versions require permission to write log files
         if (ContextCompat.checkSelfPermission(
@@ -233,6 +244,23 @@ private fun writeExternalFile(
     return outputFilename
 }
 
+//
+private fun readChangeFile(context: Context): String {
+    val assetManager = context.assets
+    val message = StringBuilder()
+    try {
+        val inStream = assetManager.open("info.txt")
+        val reader = BufferedReader(InputStreamReader(inStream))
+        var line: String?
+        while (reader.readLine().also { line = it } != null) {
+            message.append(line + "\n")
+        }
+    } catch (_: Exception) {
+    }
+    return message.toString()
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainApplication(modifier: Modifier = Modifier) {
     val context = LocalContext.current
@@ -243,13 +271,60 @@ fun MainApplication(modifier: Modifier = Modifier) {
     var permissions by remember { mutableStateOf(notificationManager.isNotificationPolicyAccessGranted) }
     var isTextVisible by remember { mutableStateOf(storage.textVisible) }
     var isTextDescriptive by remember { mutableStateOf(storage.textDescription) }
-
+    var selectedIndex by remember { mutableIntStateOf(0) }
+    var showDialog by remember { mutableStateOf(storage.newInstall) }
     val launcher =
         rememberLauncherForActivityResult(ActivityResultContracts.StartActivityForResult())
         {
             permissions = notificationManager.isNotificationPolicyAccessGranted
         }
-    var selectedIndex by remember { mutableIntStateOf(0) }
+
+    // If the app has been updated, display a dialog explaining changes
+    if (showDialog) {
+        BasicAlertDialog(
+            onDismissRequest = {
+                storage.newInstall = false
+                showDialog = false
+            },
+        ) {
+            Surface(
+                modifier = Modifier
+                    .widthIn(max = 300.dp)
+                    .wrapContentHeight(),
+                shape = MaterialTheme.shapes.large,
+            ) {
+                Column(
+                    modifier = Modifier
+                        .padding(24.dp)
+                        .verticalScroll(rememberScrollState()),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = stringResource(R.string.whats_new) + BuildConfig.VERSION_NAME,
+                        fontWeight = FontWeight.Bold,
+                    )
+                    HorizontalDivider(thickness = 2.dp)
+                    // Get the description from the asset file
+                    val description = readChangeFile(context)
+                    Text(text = description)
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.End
+                    ) {
+                        Spacer(modifier = Modifier.width(8.dp))
+                        TextButton(
+                            onClick = {
+                                showDialog = false
+                                storage.newInstall = false
+                            }
+                        ) {
+                            Text(stringResource(R.string.dismiss_button))
+                        }
+                    }
+                }
+            }
+        }
+    }
 
     Column(
         modifier = modifier
@@ -591,6 +666,7 @@ fun MainApplication(modifier: Modifier = Modifier) {
         }
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
